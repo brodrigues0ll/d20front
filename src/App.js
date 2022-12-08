942ffbbee4777f5096e573a1023d310574cc3e42
brodrigues0ll <bernardorg99@hotmail.com>
2022-12-08 19:40:56 -0300

    test: Foi feita configuração inicial do Frontend para uso de styled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
@@ -6,10 +5,10 @@ import SignIn from './pages/SignIn';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signin" element={<SignIn />} />
+    </Routes>
 
   );
 }
